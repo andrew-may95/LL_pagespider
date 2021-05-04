2b5dbb85b85321afedbefa00d024bff0b5ded7e4
Andrew May <andrew.may@methods.co.uk>
2021-05-04 14:57:20 +0100

    Finishing chapter 1 on LL: Python with PyCharm

---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+
+def load_urls_from_file(file_path: str):
+    #TODO: add code to read txt file containing urls
+    pass
+
+def load_page(url: str):
+    #TODO: add code to read the url
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
