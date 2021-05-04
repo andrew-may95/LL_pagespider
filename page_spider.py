eb27e819836f9ce3ac59d8798931fc7500b1e00b
Andrew May <andrew.may@methods.co.uk>
2021-05-04 16:08:12 +0100

    Fixed formatting

--- page_spider.py
+++ page_spider.py
@@ -2,6 +2,7 @@
 import os
 import argparse
 
+
 def main(database: str, url_list_file: str):
     print("we are going to work with " + database)
     print("we are going to scan " + url_list_file)
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,12 +1,13 @@
-
 def load_urls_from_file(file_path: str):
-    #TODO: add code to read txt file containing urls
+    # TODO: add code to read txt file containing urls
     pass
+
 
 def load_page(url: str):
-    #TODO: add code to read the url
+    # TODO: add code to read the url
     pass
 
+
 def scrape_page(page_contents: str):
-    #TODO: analyze the text
+    # TODO: analyze the text
     pass
